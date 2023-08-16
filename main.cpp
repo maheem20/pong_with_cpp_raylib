1f784467d232464812df2157987cfad5cf62ad8c
Maheem <88165826+maheem20@users.noreply.github.com>
2023-08-16 20:44:15 +0000

    set refresh rate to 60 fps

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main () {
     const int screenWidth = 1280;
     const int screenHeight = 800;
     InitWindow(screenWidth, screenHeight, "My Pong Game!");
+    SetTargetFPS(60);
 
     while (!WindowShouldClose() == false) {
         BeginDrawing();
