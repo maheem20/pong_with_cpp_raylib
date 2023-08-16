a444bf9c5300e403bddfb241efb7df3b5df0d1c7
Maheem <88165826+maheem20@users.noreply.github.com>
2023-08-16 21:10:53 +0000

    move DrawCircle function to Ball class

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ class Ball {
         float x, y;
         int speed_x, speed_y;
         int radius;
+
+    void Draw() {
+        DrawCircle(x, y, radius, WHITE);
+    }
 };
 
 int main () {
@@ -21,7 +25,6 @@ int main () {
 
         #Drawing
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
-        DrawCircle(screenWidth / 2, screenHeight / 2, 20, WHITE);
         DrawRectangle(10, screenHeight / 2 - 60, 25, 120, WHITE);
         DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE);
 
